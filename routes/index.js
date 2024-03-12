1f244ae4325c79f584fda19e8cdd037291cfca2f
IanKiguru <iankiguru72@outlook.com>
2024-03-12 03:44:41 -0700

    alx-files_manager

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+import UsersController from '../controllers/UsersController';
+
+
+const router = Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
+
+module.exports = router;
